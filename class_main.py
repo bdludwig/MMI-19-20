a0ad2ec4ba490e7a13ef94118fee69e3e5e16a3e
Sabine Roth <sabi.roth@t-online.de>
2019-11-10 22:35:30 +0100

    Revert "Extending code and renaming functions"
    This reverts commit 456d7447e7d7fa84546238d3e72e1c7a017b37a3.

--- class_main.py
+++ class_main.py
@@ -1,15 +1,15 @@
-import class_appliance
-import class_furniture
+import class_gerate
+import class_moebel
 
 
 if __name__ == '__main__':
 
-    a = class_appliance.kaffemaschine() #Neue Kaffemaschine erstellt (neues Objekt)
+    a = class_gerate.kaffemaschine() #Neue Kaffemaschine erstellt (neues Objekt)
     a.kaffeKochen() #Methode Kaffekochen aufgerufen
     a.aufheizen()
-    b = class_appliance.wasserkocher()
+    b = class_gerate.wasserkocher()
     b.aufheizen()
-    c = class_furniture.unterschrank()
+    c = class_moebel.unterschrank()
     c.statusWiedergeben()
     b.eigenschaften()
     b.x = True
